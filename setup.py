f274747fbc919b2e0bb326a62ebbacd983591be2
tony-h <tony-h@users.noreply.github.com>
2022-08-24 14:44:37 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description="Integrates Rocket.Chat into each course by adding a new tab.",
     url="https://github.com/tony-h/rocketchat-tab",
     packages=setuptools.find_packages(),
+    include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
